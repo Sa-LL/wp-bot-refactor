e754c57de5ac098dfe9a31c6c0d3e6fb1d8212d4
Santiago Londoño <santiago.londono@simetrik.com>
2022-12-14 14:49:02 -0500

    changes

--- browser.js
+++ browser.js
@@ -1,17 +1,20 @@
-import puppeteer from 'puppeteer';
-import fs from 'fs';
-import * as SELECTORS from './selectors.js';
+import puppeteer from "puppeteer";
+import fs from "fs";
+import * as SELECTORS from "./selectors.js";
 
 const isHeadless = true;
 
 const delay = 3000;
 // await new Promise((r) => setTimeout(r, 1000));
 
-const SS_PATH = './screenshots/';
+const EXIT_TEXT = '!exit';
+
+const SS_PATH = "./screenshots/";
 
 const SS_OBJECT = {
-  QR_CODE: 'qr.png',
-  DRAWER_LEFT: 'drawer_left.png',
+  QR_CODE: "qr.png",
+  DRAWER_LEFT: "drawer_left.png",
+  INPUT: 'input.png'
 };
 
 /**
@@ -23,16 +26,32 @@ Object.keys(SS_OBJECT).forEach((key) => {
   });
 });
 
+function getRandom(min, max) {
+  const floatRandom = Math.random()
+
+  const difference = max - min
+
+  // random between 0 and the difference
+  const random = Math.round(difference * floatRandom)
+
+  const randomWithinRange = random + min
+
+  return randomWithinRange
+}
+
+const contactNameTemporal = "C&D: Cena navideña 🎅🏻";
+// const contactNameTemporal = "Audios";
+
 (async () => {
   const browser = await puppeteer.launch({ headless: isHeadless });
 
   const page = await browser.newPage();
 
   await page.setUserAgent(
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
   );
 
-  await page.goto('https://web.whatsapp.com');
+  await page.goto("https://web.whatsapp.com");
 
   await page.waitForSelector(SELECTORS.QR_CODE);
 
@@ -44,13 +63,39 @@ Object.keys(SS_OBJECT).forEach((key) => {
 
   await page.screenshot({ path: SS_PATH + SS_OBJECT.DRAWER_LEFT });
 
-  const unreadElements = await page.$$(SELECTORS.UNREAD_CLASS);
+  await new Promise((r) => setTimeout(r, 1000));
 
-  unreadElements.forEach((element) => {
-    element.click();
-  });
+  await page.click(`span[title="${contactNameTemporal}"]`);
 
-  console.log(unreadElements);
+  let lastMessageSpan = '';
+
+  while(lastMessageSpan !== EXIT_TEXT) {
+
+    await new Promise((r) => setTimeout(r, 1000));
+      
+    const messagesOnScreen = await page.$$(SELECTORS.MESSAGES);
+  
+    const lastMessage = messagesOnScreen.pop();
+  
+    lastMessageSpan = await lastMessage.$eval("span", element => element.textContent);
+
+    if (lastMessageSpan === '!d20') {
+      const D20 = getRandom(1, 20);
+      await page.type(SELECTORS.INPUT_MESSAGE, `D20: ${D20}`);
+      await page.click(SELECTORS.SEND_BUTTON);
+    }
+
+    console.log(`El último elemento span es: ${lastMessageSpan}`);
+
+  }
+
+  // const unreadElements = await page.$$(SELECTORS.UNREAD_CLASS);
+
+  // unreadElements.forEach((element) => {
+  //   element.click();
+  // });
+
+  // console.log(unreadElements);
 
   await browser.close();
 })();
--- selectors.js
+++ selectors.js
@@ -2,4 +2,11 @@ export const QR_CODE = 'div[data-testid="qrcode"]';
 
 export const DRAWER_LEFT = 'div[data-testid="drawer-left"]';
 
-export const UNREAD_CLASS = '.ovhn1urg';
+export const UNREAD_CLASS = ".ovhn1urg";
+
+export const INPUT_MESSAGE = "p.selectable-text.copyable-text";
+
+export const MESSAGES =
+  'div[data-testid="msg-container"] .selectable-text.copyable-text';
+
+export const SEND_BUTTON = 'span[data-testid="send"]';
